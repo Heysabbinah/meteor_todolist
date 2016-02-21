af5e85ca5e86822e2662a57b41bf0fa8b1cd895d
heysabbinah <dubysound@hotmail.com>
2016-02-21 16:01:35 +0100

    add the form the update and remove tasks

---
+++ simple-todos.js
@@ -0,0 +1,60 @@
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
+  });
+}
+
+/* ----------- DEFINE COLLECTION ----------*/
+
+Tasks = new Mongo.Collection("tasks");
+ 
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+/* ----------- DEFINE SUBMIT (avec le entree) ----------*/
+
+Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
+
+/* ----- Event handlers for Task buttons update or remove task ----------*/
+
+Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
+
+}
