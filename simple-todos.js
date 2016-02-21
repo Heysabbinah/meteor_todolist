8d420e1217f285f2417bbf533239874ed26f797c
heysabbinah <dubysound@hotmail.com>
2016-02-21 19:09:23 +0100

    add Filtering data with publish and subscribe only owner can delete a task or check off a private task

--- simple-todos.js
+++ simple-todos.js
@@ -12,9 +12,24 @@ if (Meteor.isClient) {
 /* ----------- DEFINE COLLECTION ----------*/
 
 Tasks = new Mongo.Collection("tasks");
+if (Meteor.isServer) {
+  // This code only runs on the server
+
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish("tasks", function () {
+     return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
+  });
+}
  
 if (Meteor.isClient) {
   // This code only runs on the client
+  Meteor.subscribe("tasks");
+
   Template.body.helpers({
     tasks: function () {
       // function qui montrera que les task qui ne sont pas coches
@@ -46,10 +61,8 @@ Template.body.events({
       var text = event.target.text.value;
  
       // Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
+      Meteor.call("addTask", text);
+      
  
       // Clear form
       event.target.text.value = "";
@@ -61,24 +74,85 @@ Template.body.events({
     }
 
   });
+/* ---------  Define helper to check ownership ------*/
+Template.task.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    }
+  });
 
 /* ----- Event handlers for Task buttons update or remove task ----------*/
-
 Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {
-        $set: {checked: ! this.checked}
-      });
+      Meteor.call("setChecked", this._id, ! this.checked);
     },
     "click .delete": function () {
-      Tasks.remove(this._id);
+       Meteor.call("deleteTask", this._id);
+       },
+
+    /* ------- Add event handler to call the setPrivate method ---*/   
+    "click .toggle-private": function () {
+      Meteor.call("setPrivate", this._id, ! this.private);
     }
   });
-/* ----- configure the accounts UI to use usernames instead of email addresses ----------*/
+/* ----- Configure accounts-ui ----------*/
 
 Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
 
 }
+
+/* ----- Define some methods ----------*/
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+ 
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+
+// Add some extra security to methods - only the owner
+var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+
+// Add some extra security to methods
+var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+    },
+
+  /*---- Define method to set tasks to private ------*/
+
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+ 
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+ 
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+  }
+});
